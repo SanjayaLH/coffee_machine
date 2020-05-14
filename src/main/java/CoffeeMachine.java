7cc16f1cc9ac31ef2a52edaf44373daf8cc46739
sanjaya <sanjayahalwala@gmail.com>
2020-05-14 13:37:13 +0530

    Compleated the stage 3

--- src/main/java/CoffeeMachine.java
+++ src/main/java/CoffeeMachine.java
@@ -3,25 +3,52 @@ import java.util.Scanner;
 public class CoffeeMachine {
 
     public static void main(String[] args) {
-        System.out.println("Write how many cups of coffee you will need:");
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int amountOfWater = scanner.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int amountOfMilk = scanner.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int amountOfCoffee = scanner.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
         int numberOfCups = scanner.nextInt();
+
         String msg = String.format("For %s cups of coffee you will need:", numberOfCups);
         System.out.println(msg);
-        calculateAmounts(numberOfCups);
+
+        calculateAmounts(numberOfCups, calculateDeliverableNumberOfCups(amountOfWater, amountOfMilk, amountOfCoffee));
     }
 
-    private static void calculateAmounts(int numberOfCups) {
-        int amountOfWater = numberOfCups * 200;
-        int amountOfMilk = numberOfCups * 50;
-        int amountOfCoffeeBeans = numberOfCups * 15;
+    //needs 200 ml of water, 50 ml of milk, and 15 g of coffee beans
+    private static int calculateDeliverableNumberOfCups(int waterInput, int milkInput, int coffeeBeanInput) {
+        int water, milk, coffeeBean, deliverableNumberOfCups;
 
-        String msgWater = String.format("%s ml of water", amountOfWater);
-        String msgMilk = String.format("%s ml of milk", amountOfMilk);
-        String msgCoffee = String.format("%s g of coffee beans", amountOfCoffeeBeans);
+        if (waterInput >= 200 || milkInput >= 50 || coffeeBeanInput >= 15) {
+            water = waterInput / 200;
+            milk = milkInput / 50;
+            coffeeBean = coffeeBeanInput / 15;
+            deliverableNumberOfCups = Math.min(water, milk);
+            deliverableNumberOfCups = Math.min(deliverableNumberOfCups, coffeeBean);
+        } else {
+            deliverableNumberOfCups = 0;
+        }
+        return deliverableNumberOfCups;
+    }
 
-        System.out.println(msgWater);
-        System.out.println(msgMilk);
-        System.out.println(msgCoffee);
+    private static void calculateAmounts(int numberOfCupsNeeded, int numberOfCupsCanDeliver) {
+        if (numberOfCupsNeeded == numberOfCupsCanDeliver) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (numberOfCupsNeeded > numberOfCupsCanDeliver) {
+            String msg = String.format("No, I can make only %s cup(s) of coffee", numberOfCupsCanDeliver);
+            System.out.println(msg);
+        } else {
+            int excessCups = numberOfCupsCanDeliver - numberOfCupsNeeded;
+            String msg = String.format("Yes, I can make that amount of coffee (and even %s more than that)", excessCups);
+            System.out.println(msg);
+        }
     }
 }
